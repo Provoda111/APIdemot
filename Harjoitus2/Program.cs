b9a8ac8f8b981d0b90fdad4e09f83df00fdfaac9
Provoda111 <provodochek2@gmail.com>
2024-10-02 12:59:51 +0300

    Harjoitus 2
    Harjoitus 2 aloitettu, Sain POST ja kaksi GET

---
+++ Harjoitus2/Program.cs
@@ -0,0 +1,55 @@
+using Harjoitus2;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<QuestDb>(opt => opt.UseInMemoryDatabase("QuestList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+var questItems = app.MapGroup("/quests");
+
+/*quests.MapGet("/", GetAllQuests);
+quests.MapGet("/{id}", GetQuest);
+quests.MapPost("/", CreateQuest);*/
+
+app.MapGet("/quests", async (QuestDb db) =>
+    await db.Quests.ToListAsync()
+);
+
+app.MapGet("/quests/{id}", async (int id, QuestDb db) =>
+    await db.Quests.FindAsync(id)
+        is Quest quest
+            ? Results.Ok(quest)
+            : Results.NotFound()
+);
+app.MapPost("/quests", async (Quest quest, QuestDb db) =>
+{   
+    db.Quests.Add(quest);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/quests/{quest.Id}", quest);
+});
+
+
+app.Run();
+
+
+
+/*static async Task<IResult> GetAllQuests(QuestDb db)
+{
+    return TypedResults.Ok(await db.Quests.ToArrayAsync());
+}
+static async Task<IResult> GetQuest(int id, QuestDb db)
+{
+    return await db.Quests.FindAsync(id)
+        is Quest quest
+            ? TypedResults.Ok(quest)
+            : TypedResults.NotFound();
+}
+static async Task<IResult> CreateQuest(Quest quest, QuestDb db)
+{
+    db.Quests.Add(quest);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/questitems/{quest.Id}", quest);
+}*/
---
+++ Harjoitus2/QuestDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Harjoitus2
+{
+    public class QuestDb : DbContext
+    {
+        public QuestDb(DbContextOptions<QuestDb> options)
+        : base(options) { }
+
+        public DbSet<Quest> Quests => Set<Quest>();
+    }
+}
